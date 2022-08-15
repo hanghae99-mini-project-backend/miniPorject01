c0e8ebacb4693cd3144bf3a6598b6f8515fedbfe
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-08-16 00:08:24 +0900

    routes/comment.js 의 comment create router 유저 인증 미들웨어 추가

--- routes/comment.js
+++ routes/comment.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Comment = require("../controller/comment");
+const authenticateUser = require("../middleware/authentication");
 
-router.route("/:postId").post(Comment.createComment);
+router.route("/:postId").post(authenticateUser, Comment.createComment);
 
 module.exports = router;
