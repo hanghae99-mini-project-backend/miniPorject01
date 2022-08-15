d1fa37eeb64c99d34e4635d442460d0dc557dd43
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-08-15 22:59:02 +0900

    comment 수정

--- controller/comment.js
+++ controller/comment.js
@@ -6,7 +6,7 @@ const comment = new Comment();
 
 exports.createComment = (req, res) => {
     const postId = req.params.postId;
-    const userIdx = req.locals.user.USER_IDX;
+    const userIdx = req.locals.user;
     const newComment = {
       userIdx: userIdx,
       bootcampIdx : postId,
