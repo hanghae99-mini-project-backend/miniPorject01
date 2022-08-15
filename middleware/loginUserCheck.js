651d8010dd028d5b7dc7fd1128ba6d664a9a0ec7
CalvinParkNov <dev.calvinpark@gmail.com>
2022-08-15 21:01:36 +0900

    loginUserCheck added

--- middleware/loginUserCheck.js
+++ middleware/loginUserCheck.js
@@ -1,9 +1,13 @@
 //로그인 유효성
 const { loginError } = require("../errors");
 module.exports = (req, res, next) => {
-  const cookies = req.cookies[process.env.COOKIE_NAME];
-  if (cookies) {
-    throw new loginError("이미 로그인이 되어있습니다.");
+  try {
+    const cookies = req.cookies[process.env.COOKIE_NAME];
+    if (cookies) {
+      throw new loginError("이미 로그인이 되어있습니다.");
+    }
+    next();
+  } catch {
+    throw new loginError("잘못된 접근입니다.");
   }
-  next();
 };
